27b9532cc60757ac6c10039e4cd45c5ad71e1759
mayank-chaurasiya <try.maynk@gmail.com>
2026-02-17 00:49:48 +0530

    sign up or sign in page

---
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -0,0 +1,48 @@
+import { assets } from "../../assets/assets";
+import "./LoginPopup.css";
+import { useState } from "react";
+
+const LoginPopup = ({ setShowLogin }) => {
+  const [currState, setCurrState] = useState("Log in");
+  return (
+    <div className="login-popup">
+      <form className="login-popup-container">
+        <div className="login-popup-title">
+          <h2>{currState}</h2>
+          <img
+            onClick={() => setShowLogin(false)}
+            src={assets.cross_icon}
+            alt=""
+          />
+        </div>
+        <div className="login-popup-inputs">
+          {currState === "Log in" ? (
+            <></>
+          ) : (
+            <input type="text" placeholder="Your Name" required />
+          )}
+          <input type="email" placeholder="Your email" required />
+          <input type="password" placeholder="Password" required />
+        </div>
+        <button>{currState === "Sign Up" ? "Create account" : "Log in"}</button>
+        <div className="login-popup-condition">
+          <input type="checkbox" required />
+          <p>By continuing, I agree to the terms of use & privacy policy.</p>
+        </div>
+        {currState === "Log in" ? (
+          <p>
+            Create a new account?{" "}
+            <span onClick={() => setCurrState("Sign Up")}>Click here</span>
+          </p>
+        ) : (
+          <p>
+            Already have an account?{" "}
+            <span onClick={() => setCurrState("Log in")}>Log in</span>
+          </p>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default LoginPopup;
