891b327acf1599977d249c555ec2ad2075d80032
mayank-chaurasiya <try.maynk@gmail.com>
2026-02-26 17:54:31 +0530

    setting up placeorder

---
+++ backend/controllers/order.controller.js
@@ -0,0 +1,56 @@
+import orderModel from "../models/order.model.js";
+import userModel from "../models/user.model.js";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+// placing user order from frontend
+const placeOrder = async (req, res) => {
+  const frontend_url = `http://localhost:5173`;
+  try {
+    const newOrder = new orderModel({
+      userId: req.body.userId,
+      items: req.body.items,
+      amount: req.body.amount,
+      address: req.body.address,
+    });
+    await newOrder.save();
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData: {} });
+
+    const line_Items = req.body.items.map((item) => ({
+      price_data: {
+        currency: "inr",
+        product_data: {
+          name: item_name,
+        },
+        unit_amount: item.price * 100 * 80,
+      },
+      quantity: item.quantity,
+    }));
+
+    line_Items.push({
+      price_data: {
+        currency: "inr",
+        product_data: {
+          name: "Delivery Charges",
+        },
+        unit_amount: 2 * 100 * 80,
+      },
+      quantity: 1,
+    });
+
+    const session = await stripe.checkout.session.create({
+      line_Items: line_Items,
+      mode: "payment",
+      success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+    });
+
+    res.json({ success: true, session_url: session.url });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+export { placeOrder };
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/food.route.js";
 import userRouter from "./routes/user.route.js";
 import cartRouter from "./routes/cart.route.js";
+import orderRouter from "./routes/order.route.js";
 
 // app config
 const app = express();
@@ -21,6 +22,7 @@ app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
   res.send("API WORKING");
